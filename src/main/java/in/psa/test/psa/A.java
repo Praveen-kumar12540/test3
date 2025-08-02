43834c736ae3b435ff93d46a30c7947bb4a67727
sonalraw7@gmail.com <sonalraw7@gmail.com>
2025-08-02 16:28:24 +0530

    df

--- src/main/java/in/psa/test/psa/A.java
+++ src/main/java/in/psa/test/psa/A.java
@@ -1,10 +1,19 @@
 package in.psa.test.psa;
 
 public class A {
+<<<<<<< Updated upstream
 =================
 kjdhfdkaf
 ================sd
 skdha========
+=======
+features 1
+login
+===============
+
+
+
+>>>>>>> Stashed changes
   int s=100;
   int b = 200;
   int c = 5000;
